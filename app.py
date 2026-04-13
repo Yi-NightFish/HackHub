b8bc4584d49173213862d3bf775b2144c139fea1
Hong-git-hub <nxinhong@gmail.com>
2026-04-14 01:00:27 +0800

    try migrate

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
+
+migrate = Migrate(app, db)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hackhub.db'
