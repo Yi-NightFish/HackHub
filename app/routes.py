f34319012961d4ddc192764e50d573513fd5691c
soonHong628 <soonhong2007@gmail.com>
2026-04-13 17:34:04 +0800

    Made profile page rendered based on user id

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,17 @@
-from app import app
+from app import app, db
 from flask import render_template
 from app.forms import ProfileForm
+from app.models import User
 
 @app.route("/")
 @app.route("/index")
 def home():
     return render_template("home.html")
 
-@app.route("/profile")
-def profile():
+@app.route("/profile/<user_id>", methods = ["GET", "POST"])
+def profile(user_id):
     profile_page = ProfileForm()
-    return render_template("profile.html", form = profile_page)
+    user = db.session.get(User, user_id)
+
+    # Mock that user has signed in. Remove current_user argument when using flask_login extension because current_user argument is automatically set
+    return render_template("profile.html", form = profile_page, user = user, current_user = user)
