ce054025addde3004c37e31dc100f2508c97129f
Wan Yi <tanwanyi.920@gmail.com>
2026-04-20 22:40:02 +0800

    clean

--- config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = "hackhub-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = "x7186460@gmail.com"
-    MAIL_PASSWORD = "chon petb yizr wcrt"
