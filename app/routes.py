825484645eea53f66dcd17a20a76fb200fe0762c
Hong-git-hub <nxinhong@gmail.com>
2026-04-20 18:02:12 +0800

    add chat_routes

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db, Messages, User
+
+def chat_routes(app):
+    @app.route("/")
+    def chat():
+        if "messages" not in session:
+            session["messages"] = []
+        return render_template("chat.html", message=session["messages"])
+    
+    @app.route("/send_message", methods=["POST"])
+    def send_message():
+        message = request.form["message"]
+        session["messages"].append(message)
+        return redirect(url_for("chat"))
+    
+    @app.route("/clear")
+    def clear_messages():
+        session["messages"] = []
+        return redirect(url_for("chat"))
