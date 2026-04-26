32fbbfc2c3ec9b302bacd651fd37ed8d72e32515
Wan Yi <tanwanyi.920@gmail.com>
2026-04-26 21:30:21 +0800

    move email config to env variables

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "hehehehe"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     from app import app
     os.makedirs(app.instance_path, exist_ok = True)
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(app.instance_path, "hackhub.db")
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "x7186460@gmail.com"
-    MAIL_PASSWORD = "chon petb yizr wcrt"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
