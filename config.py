19d7bd7e27951df97347bea3f19e85e1a2f7a70d
soonHong628 <soonhong2007@gmail.com>
2026-05-03 13:28:11 +0800

    Put some sensitive keys from config.py to an environemnt file.

--- config.py
+++ config.py
@@ -9,8 +9,6 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "x7186460@gmail.com"
-    MAIL_PASSWORD = "chon petb yizr wcrt"
     from functools import reduce
     app.jinja_env.globals["reduce"] = reduce
     app.jinja_env.globals["count_participants"] = lambda total, team : total + len(team.members)
